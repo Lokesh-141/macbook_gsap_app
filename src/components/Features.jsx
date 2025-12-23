e84a72280806318f720150c995216c293878694e
@Lokesh-141 <kumarilokesh141@gmail.com>
2025-12-23 18:46:24 +0530

    implement footer

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -54,7 +54,8 @@ const ModelScroll = () => {
             modelTimeline.to(groupRef.current.rotation, { y: Math.PI * 2, ease: 'power1.inOut'})
         }
 
-        timeline.call(() => setTexture('/videos/feature-1.mp4')) .to('.box1', { opacity: 1, y: 0, delay: 1 })
+        timeline
+            .call(() => setTexture('/videos/feature-1.mp4')) .to('.box1', { opacity: 1, y: 0, delay: 1 })
             .call(() => setTexture('/videos/feature-2.mp4')).to('.box2', { opacity: 1, y: 0 })
             .call(() => setTexture('/videos/feature-3.mp4')).to('.box3', { opacity: 1, y: 0 })
             .call(() => setTexture('/videos/feature-4.mp4')).to('.box4', { opacity: 1, y: 0})
